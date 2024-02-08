4e2eec52b23e89d57a67fee591206ab8a5600a14
Krzysztof <krzysztofnalewajko59@gmail.com>
2024-02-08 21:55:38 +0100

    Zadanie 5

--- NewChallenge/NewChallenge/Program.cs
+++ NewChallenge/NewChallenge/Program.cs
@@ -1,5 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
-var num = 13;
-Console.WriteLine(num);
+var number = "72723339";
+
+var decimalDigits = "0123456789";
+
+List<int> listOfDecimalDigits = new List<int>();
+
+List<int> numberOfRepetitionsDigit = new List<int>();
+
+for (int i = 0; i < 10; i++)
+{
+    listOfDecimalDigits.Add(i);
+    numberOfRepetitionsDigit.Add(0);
+}
+
+foreach (int digit in listOfDecimalDigits)
+{
+    foreach (char num in number)
+    {
+        var valueNumber = int.Parse(num.ToString());
+
+        if (digit == valueNumber)
+        {
+            numberOfRepetitionsDigit[digit] = numberOfRepetitionsDigit[digit] + 1;
+        }
+    }
+    Console.WriteLine($"{listOfDecimalDigits[digit]}  =>  {numberOfRepetitionsDigit[digit]}");
+}
+
+
