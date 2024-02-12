af6a1518d2ba12695cfb47dbec03c8203549cb26
Krzysztof <krzysztofnalewajko59@gmail.com>
2024-02-12 05:11:21 +0100

    Testy

---
+++ Lesson_6/Lesson_6.Test/UnitTest1.cs
@@ -0,0 +1,21 @@
+using ChallengeApp2024;
+
+namespace Lesson_6.Test
+{
+    public class Tests
+    {
+
+        [Test]
+        public void Test1()
+        {
+            //arrange
+            var employee = new Employee("Kazimierz", "Dolny", 38);
+            
+            //act
+            
+            //assert
+
+            Assert.Pass();
+        }
+    }
+}
